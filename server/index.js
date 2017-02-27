b3551bbd9cb39be604fa5f285e1cb3e7cd40f55e
Alex <olefirenk@gmail.com>
2017-02-27 11:25:27 +0200

    start script fix for windows

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('babel-register');
 ['.css', '.less', '.sass', '.ttf', '.woff', '.woff2', '.jpg', '.png'].forEach((ext) => require.extensions[ext] = () => {
-});
-require('./server');
+})
+process.env.NODE_ENV = 'development'
+require('./server')
