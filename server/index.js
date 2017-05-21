2c39f4989b0650b81671f531438595c32998fd45
Alex <alex@mjlt2s-MacBook-Pro.local>
2017-05-21 17:20:20 +0300

    was remove unneeded file

--- server/index.js
+++
@@ -1,6 +0,0 @@
-require('babel-register');
-require('babel-polyfill');
-['./css', '.less', '.sass', '.ttf', '.woff', '.woff2', '.jpg', '.png'].forEach((ext) => require.extensions[ext] = () => {
-})
-process.env.NODE_ENV = 'development'
-require('./server')
