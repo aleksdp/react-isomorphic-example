6d8ce9eca71afcff6ee1803b90121033cae8c511
Alex <alex@alex-MacBook-Pro.local>
2017-06-30 14:07:12 +0300

    fix

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import {routerMiddleware} from 'react-router-redux'
 import {loadingBarMiddleware} from 'react-redux-loading-bar'
 import rootReducer from '../reducers/rootReducer'
-import {history} from '../index'
+import {history} from '../'
 import {PRELOAD_START, PRELOAD_SUCCESS, PRELOAD_FAIL} from 'react-isomorphic-tools/constants'
 import preload from 'react-isomorphic-tools/middlewares/preload'
 
@@ -11,11 +11,12 @@ import preload from 'react-isomorphic-tools/middlewares/preload'
 export default function configureStore() {
     const composeEnhancers = typeof window == 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
     const store = composeEnhancers(
-        applyMiddleware(thunk),
-        applyMiddleware(preload(history)),
-        applyMiddleware(routerMiddleware(history)),
-        applyMiddleware(loadingBarMiddleware({promiseTypeSuffixes: [PRELOAD_START, PRELOAD_SUCCESS, PRELOAD_FAIL]})),
-    )(createStore)(rootReducer, typeof window == 'object' ? window.__data : {})
+        applyMiddleware(
+            thunk,
+            preload(history),
+            routerMiddleware(history),
+            loadingBarMiddleware({promiseTypeSuffixes: [PRELOAD_START, PRELOAD_SUCCESS, PRELOAD_FAIL]})
+        ))(createStore)(rootReducer, typeof window == 'object' ? window.__data : {})
 
     if (module.hot) {
         module.hot.accept('../reducers/rootReducer', () => {
