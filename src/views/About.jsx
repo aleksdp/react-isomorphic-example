a0cf022e2120edc9b6ad70dc5c29271d890752a4
Alex <olefirenk@gmail.com>
2017-08-29 22:52:50 +0300

    update

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import Readme from '../../README.md'
+import Readme from 'react-isomorphic-tools/README.md'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    div{
+        width: 90%;
+        background: transparent;
+    }
+`
 
 export default class About extends React.Component {
     render() {
         return (
-            <div dangerouslySetInnerHTML={{__html: Readme}} className='markdown'/>
+            <Wrapper>
+                <div dangerouslySetInnerHTML={{__html: Readme}} className='github'/>
+            </Wrapper>
         )
     }
 }
